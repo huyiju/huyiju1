425468331e4fc2980ca03dc17f8581f741469fb3
huyiju <790270807@qq.com>
2020-05-08 19:34:09 +0800

    第一次创建test1

---
+++ Test1.java
@@ -0,0 +1,12 @@
+/**
+ * @author :huyiju
+ * @date :2020-05-01 19:35
+ */
+public class Test1 {
+    public static void main(String[] args) {
+      //  User1 u=new User1();
+    	        System.out.println("测试");
+
+
+    }
+}
